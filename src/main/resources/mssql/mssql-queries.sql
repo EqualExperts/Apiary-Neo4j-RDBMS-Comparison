0d56dca98e9b40a3463b14c32cfb2261302b64ea
Anuj Mathur <amathur@equalexperts.com>
2013-07-23 17:34:33 +0530

    1). updated query scripts with higher level queries\n 2). updated comparision report

--- src/main/resources/mssql/mssql-queries.sql
+++ src/main/resources/mssql/mssql-queries.sql
@@ -344,3 +344,22 @@ UNION
     GROUP BY manager.directly_manages    
 ) AS T
 GROUP BY T.directReportees;
+
+#traversal query - details of reportees till depth 6 reporting to "first1 last1"
+SELECT *
+    FROM person_direct_reportee manager 
+    JOIN person_direct_reportee depth1Reportees
+    ON manager.directly_manages = depth1Reportees.person_id
+        JOIN person_direct_reportee depth2Reportees
+        ON depth1Reportees.directly_manages = depth2Reportees.person_id
+            JOIN person_direct_reportee depth3Reportees
+            ON depth2Reportees.directly_manages = depth3Reportees.person_id
+                JOIN person_direct_reportee depth4Reportees
+                ON depth3Reportees.directly_manages = depth4Reportees.person_id
+                    JOIN person_direct_reportee depth5Reportees
+                    ON depth4Reportees.directly_manages = depth5Reportees.person_id
+						JOIN person_direct_reportee depth6Reportees
+						ON depth5Reportees.directly_manages = depth6Reportees.person_id
+    WHERE manager.person_id = (SELECT id
+                               FROM person
+                               WHERE name = 'first1 last1');
