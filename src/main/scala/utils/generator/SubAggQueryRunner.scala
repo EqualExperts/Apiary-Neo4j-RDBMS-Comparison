241f767dcf1325bf69360d4e69985fcc71b080af
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2013-08-09 10:57:35 +0530

    - Created SubNameQueryRunner and SubAggQueryRunner

---
+++ src/main/scala/utils/generator/SubAggQueryRunner.scala
@@ -0,0 +1,34 @@
+package utils.generator
+
+import utils.Mode._
+import utils.NeoDB
+
+object SubAggQueryRunner extends App with CypherQueryExecutor {
+
+  override def main(args: Array[String]) = {
+    val totalLevels = 3
+    // Get this manually from neo-shell
+    val params = Map("name" -> "first88 last136")
+
+    //	  val basePath = "D:/rnd/apiary"
+    val basePath = "/Users/dhavald/Documents/workspace/Apiary_Stable"
+    val fileUrl = basePath + "/NEO4J_DATA/apiary_100k_l" + totalLevels
+
+    implicit val neo4j = NeoDB(fileUrl, Embedded)
+    val visibilityLevel = totalLevels - 1
+    val subAggCql =
+      """
+        |start n = node:Person(name = {name})
+        |match n-[:DIRECTLY_MANAGES*0..%d]->m-[:DIRECTLY_MANAGES*1..%d]->o
+        |where n.level + %d >= m.level
+        |return m.name as Subordinate, count(o) as Total;
+      """.format(totalLevels - 1, totalLevels - 2, visibilityLevel).stripMargin
+
+    val (_, coldCacheExecTime) = execute(subAggCql, params)
+    val (_, warmCacheExecTime) = execute(subAggCql, params)
+    printf("For Level %d => Cold Cache Exec Time = %d (ms)\n", totalLevels, coldCacheExecTime)
+    printf("For Level %d => Warm Cache Exec Time = %d (ms)\n", totalLevels, warmCacheExecTime)
+    neo4j.shutdown
+  }
+}
+
---
+++ src/main/scala/utils/generator/SubNameQueryRunner.scala
@@ -0,0 +1,30 @@
+package utils.generator
+
+import utils.Mode._
+import utils.NeoDB
+
+object SubNameQueryRunner extends App with CypherQueryExecutor {
+
+  override def main(args: Array[String]) = {
+    val totalLevels = 3
+    // Get this manually from neo-shell
+    val params = Map("name" -> "first88 last136")
+
+    //	  val basePath = "D:/rnd/apiary"
+    val basePath = "/Users/dhavald/Documents/workspace/Apiary_Stable"
+    val fileUrl = basePath + "/NEO4J_DATA/apiary_100k_l" + totalLevels
+    implicit val neo4j = NeoDB(fileUrl, Embedded)
+    val visibilityLevel = totalLevels - 1
+    val subNameCql =
+      """
+        |start n = node:Person(name = {name})
+        |match p = n-[:DIRECTLY_MANAGES*1..%d]->m
+        |return nodes(p);
+      """.format(visibilityLevel).stripMargin
+      val (_, coldCacheExecTime) = execute(subNameCql, params)
+      val (_, warmCacheExecTime) = execute(subNameCql, params)
+      printf("For Level %d => Cold Cache Exec Time = %d (ms)\n", totalLevels, coldCacheExecTime)
+      printf("For Level %d => Warm Cache Exec Time = %d (ms)\n", totalLevels, warmCacheExecTime)
+      neo4j.shutdown
+  }
+}
